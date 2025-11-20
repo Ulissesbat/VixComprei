310dd217c203227f2689522b16e842e4f8ecc874
Ulisses Batista Santana <santana.gru@gmail.com>
2025-11-19 21:23:54 -0300

    Crud usuario

--- src/main/java/com/vixcomprei/controllers/UsuarioController.java
+++ src/main/java/com/vixcomprei/controllers/UsuarioController.java
@@ -2,6 +2,7 @@ package com.vixcomprei.controllers;
 
 import com.vixcomprei.dto.UsuarioDto;
 import com.vixcomprei.services.UsuarioService;
+import com.vixcomprei.services.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -25,13 +26,35 @@ public class UsuarioController {
         return ResponseEntity.ok().body(list);
     }
 
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UsuarioDto> findById(@PathVariable Long id) {
+        UsuarioDto dto = usuarioService.findById(id);
+        return ResponseEntity.ok().body(dto);
+    }
+
     @PostMapping
     public ResponseEntity<UsuarioDto> insert(@RequestBody UsuarioDto dto){
         dto = usuarioService.insert(dto);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                 .buildAndExpand(dto.getId()).toUri();
         return ResponseEntity.created(uri).body(dto);
-
     }
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<UsuarioDto> update(@PathVariable Long id, @RequestBody UsuarioDto dto) {
+        try {
+            UsuarioDto newDto = usuarioService.update(id, dto);
+            return ResponseEntity.ok().body(newDto);
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        usuarioService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
 }
 
--- src/main/java/com/vixcomprei/services/UsuarioService.java
+++ src/main/java/com/vixcomprei/services/UsuarioService.java
@@ -3,12 +3,18 @@ package com.vixcomprei.services;
 import com.vixcomprei.dto.UsuarioDto;
 import com.vixcomprei.entities.Usuario;
 import com.vixcomprei.repositories.UsuarioRepository;
+import com.vixcomprei.services.exception.DatabaseException;
+import com.vixcomprei.services.exception.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -23,6 +29,13 @@ public class UsuarioService {
         return list.map(UsuarioDto::new);
     }
 
+    @Transactional(readOnly = true)
+    public UsuarioDto findById(Long id) {
+        Optional<Usuario> obj = usuarioRepository.findById(id);
+        Usuario entity = obj.orElseThrow(() -> new ResourceNotFoundException("Id not found"));
+        return new UsuarioDto(entity);
+    }
+
 
     public UsuarioDto insert(UsuarioDto dto) {
 
@@ -30,6 +43,32 @@ public class UsuarioService {
         copyDtoToEntity(dto, entity);
         entity = usuarioRepository.save(entity);
         return new UsuarioDto (entity);
+    }
+
+    @Transactional
+    public UsuarioDto update (Long id, UsuarioDto dto) throws ResourceNotFoundException {
+
+        try {
+            Usuario entity = usuarioRepository.getReferenceById(id);
+            copyDtoToEntity(dto, entity);
+            entity = usuarioRepository.save(entity);
+            return new UsuarioDto(entity);
+        }
+        catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Id not found " + id);
+        }
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id) {
+        if (!usuarioRepository.existsById(id)) {
+            throw new ResourceNotFoundException("Id not found " + id);
+        }
+        try {
+            usuarioRepository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Integrity violation");
+        }
     }
 
     private void copyDtoToEntity(UsuarioDto dto, Usuario entity) {
